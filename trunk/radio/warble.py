78e044521983098f19968f68ae5b5481e2b1b18e
gabe.johnson <gabe.johnson@f9c4edc8-cb11-11de-9c41-d777a3202692>
2009-11-17 19:51:14 +0000

    neat warbly scifi music with GNU Radio

--- trunk/radio/warble.py
+++ trunk/radio/warble.py
@@ -3,11 +3,17 @@
 # warble.py - GNU Radio script demonstrating how to dynamically update block
 #             parameters. Every second it calculates two pitches (constrained
 #             by the command-line parameters, e.g. 200 and 800) and plays them.
-#             It produces a science-fiction-like "warble" sound.
+#             It produces a science-fiction-like "warble" sound. Give it a
+#             filename if you like, and if you do that you can also give it a
+#             number of iterations to stop after.
 #
-# Example:
+# Syntax: warble.py freq0 freq1 [filename [num_iterations]]
+#
+# Examples:
 #
 # $ ./warble.py 350 440
+# $ ./warble.py 350 440 warble_output.wav
+# $ ./warble.py 350 400 warble_output.wav 20
 
 from gnuradio import gr
 from gnuradio import audio
@@ -16,7 +22,7 @@ import sys
 import random
 
 class warble(gr.top_block):
-    def __init__(self, frq0, frq1):
+    def __init__(self, frq0, frq1, filename):
         gr.top_block.__init__(self)
 
         sample_rate = 44100
@@ -26,13 +32,14 @@ class warble(gr.top_block):
         self.src0 = gr.sig_source_f (sample_rate, gr.GR_SIN_WAVE, self.frq0, ampl)
         self.src1 = gr.sig_source_f (sample_rate, gr.GR_SIN_WAVE, self.frq1, ampl)
         self.adder = gr.add_vff(1)
-        
-        waveout = gr.wavfile_sink("warble.wav", 1, sample_rate, 8)
         dst = audio.sink (sample_rate, "", True)
         self.connect (self.src0, (self.adder, 0))
         self.connect (self.src1, (self.adder, 1))
         self.connect ((self.adder, 0), (dst, 0))
-        self.connect ((self.adder, 0), (waveout, 0))
+
+        if (filename):
+            waveout = gr.wavfile_sink(filename, 1, sample_rate, 8)
+            self.connect ((self.adder, 0), (waveout, 0))
     
     def set_tone0(self, v):
         self.src0.set_frequency(v)
@@ -40,20 +47,29 @@ class warble(gr.top_block):
     def set_tone1(self, v):
         self.src1.set_frequency(v)
 
-# The following will run the warble block    
+# The following will run the warble block
 if __name__ == '__main__':
     try:
         f_low = f0 = int(sys.argv[1])
         f_high = f1 = int(sys.argv[2])
-        thing = warble(f_low, f_high)
+        filename = None
+        num_iterations = -1
+        if (len(sys.argv) > 3):
+            filename = sys.argv[3]
+        if (len(sys.argv) > 4):
+            num_iterations = int(sys.argv[4])
+
+        thing = warble(f_low, f_high, filename)
         thing.start()
-        while (1):
+        while (num_iterations != 0):
             sleep(1)
             f0 = random.randint(f_low, f1)
             f1 = random.randint(f0, f_high)
             thing.set_tone0(f0)
             thing.set_tone1(f1)
             print (str(f0) + " " + str(f1))
+            num_iterations = num_iterations - 1
+
     except KeyboardInterrupt:
         pass
 
