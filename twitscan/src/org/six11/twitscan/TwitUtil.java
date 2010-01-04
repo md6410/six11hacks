ba2475129f7bfed78c13d365340982ec073f84a3
Dov.Kruger <Dov.Kruger@f9c4edc8-cb11-11de-9c41-d777a3202692>
2010-01-04 17:00:04 +0000

    reactored code to create a Twitscan and a Statbuilder app which collects statistics on all tweets it can get its grubby paws on

--- twitscan/src/org/six11/twitscan/TwitUtil.java
+++ twitscan/src/org/six11/twitscan/TwitUtil.java
@@ -6,17 +6,22 @@ import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 import java.util.StringTokenizer;
+import java.util.Map.Entry;
 
 import org.six11.util.io.StreamUtil;
 
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.tree.BaseTree;
 import org.antlr.runtime.tree.CommonTree;
 import org.antlr.runtime.tree.Tree;
+
+import sun.misc.Regexp;
 
 /**
  * Twitter Utility functions container
@@ -27,7 +32,8 @@ public class TwitUtil {
   private static long timerMark = 0;
   private static DecimalFormat df = new DecimalFormat("#0.00");
   public final static String FOLLOW_URL = "http://stream.twitter.com/1/statuses/filter.json";
-  private static int periodicUpdate = 50;
+  private static int periodicUpdate = 500;
+  private static int dotUpdate = 50;
 
   public static ArrayList<String> extractDesiredFields(Properties props) {
     ArrayList<String> desiredFields = new ArrayList<String>();
@@ -50,7 +56,7 @@ public class TwitUtil {
     }
   }
 
-  public static Properties loadProperties(String fileName) throws FileNotFoundException,
+  public static Properties loadProperties(String fileName, String requiredProperties[]) throws FileNotFoundException,
       IOException {
     Properties props = new Properties();
     File propFile = new File(fileName);
@@ -59,12 +65,16 @@ public class TwitUtil {
       System.exit(-1);
     }
     props.load(new FileInputStream(fileName));
-    checkProperty(props, "username");
-    checkProperty(props, "password");
-    checkProperty(props, "keywords");
-    StringTokenizer keywords = new StringTokenizer(props.getProperty("keywords"), ",");
-    while (keywords.hasMoreTokens()) {
-      System.err.println("Keyword: " + keywords.nextToken());
+    for (int i = 0; i < requiredProperties.length; i++)
+    	checkProperty(props, requiredProperties[i]);
+    
+    String keywords = props.getProperty("keywords");
+    System.err.print("Keywords: ");
+    if (keywords != null) {
+      StringTokenizer keywordTok = new StringTokenizer(keywords, ",");
+      while (keywordTok.hasMoreTokens()) {
+    	System.err.print(keywordTok.nextToken() + ' ');
+      }
     }
     if (props.containsKey("periodicUpdate")) {
       periodicUpdate = Integer.parseInt(props.getProperty("periodicUpdate"));
@@ -129,9 +139,8 @@ public class TwitUtil {
     JSONLexer lexer = new JSONLexer(input);
     CommonTokenStream tokens = new CommonTokenStream(lexer);
     JSONParser parser = new JSONParser(tokens);
-    CommonTree t;
     try {
-      t = (CommonTree) parser.jsonObject().getTree();
+      CommonTree t = (CommonTree) parser.jsonObject().getTree();
       tweetVals.setLength(0);
       for (String field : desiredFields) {
         for (int i = 0; i < t.getChildCount(); i++) {
@@ -182,6 +191,87 @@ public class TwitUtil {
     return ret.toString();
   }
 
+  private static int avgPeriod = 60 * 10 * 1000; // 10 minute averaging period
+  /**
+   * con
+   */
+  public static void connectAndCountKeywords(HttpURLConnection ht, PrintWriter logFile, HashMap<String,Integer> histogram)
+  throws IOException {
+System.err.println("Connected. Counting keywords in tweets...");
+BufferedInputStream buf = new BufferedInputStream(ht.getInputStream());
+BufferedReader bufReader = new BufferedReader(new InputStreamReader(buf));
+long t0 = System.currentTimeMillis();
+long endTime = t0 + avgPeriod;
+try {
+  while (true) {
+	long now = System.currentTimeMillis();
+	if (now > endTime) {
+		logFile.println(t0 + " ==============================");
+		for (Iterator<Entry<String, Integer>> i = histogram.entrySet().iterator(); i.hasNext(); ) {
+			Entry<String, Integer> entry = i.next();
+			logFile.println(entry.getKey() + ": " + entry.getValue());
+		}
+		t0 = endTime;
+		endTime += avgPeriod;
+		histogram.clear();
+	}
+    String tweet = bufReader.readLine();
+    if (tweet.length() > 0) {
+      countKeywords(tweet, histogram);
+    }
+    try {
+      Thread.sleep(250);
+    } catch (InterruptedException e) {
+    }
+  }
+} catch (Exception ignore) {
+}
+}
+
+private static void displayTweetStats() {
+  numTweetsProcessed++;
+  if ((numTweetsProcessed % dotUpdate) != 0)
+	  return;
+  System.err.print(".");
+  if ((numTweetsProcessed % periodicUpdate) != 0)
+	  return;
+  long now = System.currentTimeMillis();
+  long duration = now - timerMark;
+  double speed = 1000d * ((double) periodicUpdate / ((double) duration));
+  timerMark = now;
+  System.err.print(" (" + numTweetsProcessed + " tweets processed. Last " + periodicUpdate
+		  + " took " + convertTime(duration) + ", approx. " + df.format(speed)
+	      + " tweets per second)\n");
+}
+
+//private static Regexp separator = new Regexp("[\\,\\.\\!\\?\\-]+\\s*|\\s+]");
+
+public static void countKeywords(String tweet, HashMap<String,Integer> histogram) {
+if (numTweetsProcessed == 0) {
+  timerMark = System.currentTimeMillis();
+}
+
+ANTLRStringStream input = new ANTLRStringStream(tweet);
+JSONLexer lexer = new JSONLexer(input);
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+JSONParser parser = new JSONParser(tokens);
+try {
+  CommonTree t = (CommonTree) parser.jsonObject().getTree();
+  for (int i = 0; i < t.getChildCount(); i++) {
+	  String val = processNode("text", t.getChild(i));
+	  if (val != null) {
+		  String[] list = val.split("[\\,\\.\\!\\?\\-]+\\s*|\\s+]");
+		  for (int j = 0; j < list.length; j++) {
+			  histogram.put(list[j], histogram.get(list[j]) + 1);
+		  }			  
+	  }
+  }
+} catch (RecognitionException ex) {
+  ex.printStackTrace();
+}
+}
+  
+  
   private static String processNode(String field, Tree t) {
     String ret = null;
     switch (t.getType()) {
