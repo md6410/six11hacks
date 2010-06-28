66775fc32754750ff6433a886b96b16b3cf3498c
gabe.johnson <gabe.johnson@f9c4edc8-cb11-11de-9c41-d777a3202692>
2010-06-28 21:43:25 +0000

    a little progress

--- radio/prototypes/understand/python/read_program.py
+++ radio/prototypes/understand/python/read_program.py
@@ -2,8 +2,10 @@
 import sys
 import ast
 
-def explore(t):
-    print("  Exploring node named " + str(t))
+def explore(t, loc):
+#    print("  Exploring node named " + str(t))
+    if (isinstance(t, ast.Name)):
+        print("Object: " + t.id + " in context: " + str(t.ctx._attributes))
     for field in ast.iter_fields(t):
         fieldName = field[0]
         fieldVal = field[1]
@@ -12,14 +14,22 @@ def explore(t):
             for val in values:
                 if isinstance(val, ast.AST):
                     explore(val)
-                else:
-                    print("Leaf: " + str(val))
         except:
-            print(fieldName + " = " + str(fieldVal) + " (type: " + str(type(fieldVal)) + ")")
+#            print(fieldName + " = " + str(fieldVal) + " (type: " + str(type(fieldVal)) + ")")
+            pass
 
 def parseProgramString(progStr):
     tree = ast.parse(progStr)
     explore(tree)
+
+class Where:
+    def __init__(self):
+        self.file = "?"
+        self.klass = "?"
+        self.method = "?"
+
+    def report(self):
+        return ""
 
 for arg in sys.argv:
     fileObj = open(arg, "r")
