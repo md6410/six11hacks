40f93a12b5dac5188f3dd7ba88ab83dad1d43dcb
gabe.johnson@gmail.com <gabe.johnson@gmail.com@f9c4edc8-cb11-11de-9c41-d777a3202692>
2010-06-29 03:41:03 +0000

    Made an analyzer that pulls out the filename, classname, function name, and variable name. The 'raw.txt' file has a somewhat cleaned up output of this analyzer that was run on the entire GNU Radio 3.2.2 base.

--- trunk/radio/prototypes/understand/python/simple.py
+++ trunk/radio/prototypes/understand/python/simple.py
@@ -1,1 +1,17 @@
-foo.bar.connect(a, b)
+x = 3
+y = x + 3
+
+def nuts(a, b):
+    q = a + b
+    return q * q
+
+# what
+class Something:
+    def __init__(self):
+        self.blah = nuts(2,3)
+        grapefruit = "Good"
+
+    def foo():
+        x = 19
+
+q = 889
