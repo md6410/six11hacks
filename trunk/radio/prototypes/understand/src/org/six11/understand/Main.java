a72a9fb2e4e7e3d9b1aee0b9f2b46811990a2484
gabe.johnson <gabe.johnson@f9c4edc8-cb11-11de-9c41-d777a3202692>
2010-06-29 16:26:07 +0000

    Evolved the program analyzer a bit more. Post-processing is done with a Java program. Made some visualizations using manyeyes.

--- trunk/radio/prototypes/understand/src/org/six11/understand/Main.java
+++ trunk/radio/prototypes/understand/src/org/six11/understand/Main.java
@@ -1,123 +1,73 @@
 package org.six11.understand;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
-import org.antlr.runtime.ANTLRFileStream;
-import org.antlr.runtime.CharStream;
-import org.antlr.runtime.CommonTokenStream;
-import org.antlr.runtime.RecognitionException;
-import org.antlr.runtime.tree.CommonTree;
-import org.antlr.runtime.tree.CommonTreeNodeStream;
-import org.antlr.runtime.tree.Tree;
-import org.python.antlr.PythonLexer;
-import org.python.antlr.PythonParser;
-import org.python.antlr.PythonTokenSource;
-import org.python.antlr.PythonTree;
-import org.python.antlr.PythonTreeAdaptor;
-import org.python.antlr.ast.Attribute;
-import org.python.antlr.ast.Call;
-import org.python.antlr.ast.Expr;
-import org.python.antlr.ast.Name;
-import org.python.core.AstList;
 import org.six11.util.Debug;
 
 /**
  * 
  **/
 public class Main {
-  public static void main(String[] args) throws RecognitionException, IOException {
-    Debug.useColor = false;
-    CharStream cs = new ANTLRFileStream(args[0]);
-    PythonLexer lexer = new PythonLexer(cs);
-    lexer.single = true;
-    CommonTokenStream tokens = new CommonTokenStream(lexer);
-    PythonTokenSource indentedSource = new PythonTokenSource(tokens, args[0], true);
-    tokens = new CommonTokenStream(indentedSource);
-    PythonParser parser = new PythonParser(tokens, "utf-8");
-    parser.setTreeAdaptor(new PythonTreeAdaptor());
-    PythonParser.file_input_return r = parser.file_input();
-    PythonTree root = (PythonTree) r.getTree();
-    CommonTreeNodeStream nodes = new CommonTreeNodeStream(root);
-    nodes.setTokenStream(tokens);
-    processTree(root, 1);
-    // System.out.println(root.toStringTree());
+  public static void main(String[] args) throws IOException {
+    Map<String, Integer> frequencies = new HashMap<String, Integer>();
+    for (String fileName : args) {
+      bug("Processing " + fileName);
+      BufferedReader in = new BufferedReader(new FileReader(fileName));
+      while (in.ready()) {
+        String line = in.readLine();
+        int lastSpace = line.lastIndexOf("\t");
+        // String key = line.substring(0, lastSpace);
+        String val = line.substring(lastSpace + 1);
+        updateFreq(val, frequencies);
+      }
+    }
+    SortedSet<Word> words = new TreeSet<Word>();
+    for (String key : frequencies.keySet()) {
+      words.add(new Word(key, frequencies.get(key)));
+    }
+    for (Word word : words) {
+      System.out.println(word.word + "\t" + word.freq);
+    }
+  }
+
+  private static class Word implements Comparable<Word> {
+
+    String word;
+    int freq;
+
+    Word(String word, int freq) {
+      this.word = word;
+      this.freq = freq;
+    }
+
+    public int compareTo(Word o) {
+      int ret = 0;
+      if (this.freq < o.freq) {
+        ret = 1;
+      } else if (this.freq > o.freq) {
+        ret = -1;
+      } else {
+        ret = this.word.compareTo(o.word);
+      }
+      return ret;
+    }
+  }
+
+  private static void updateFreq(String val, Map<String, Integer> frequencies) {
+    if (!frequencies.containsKey(val)) {
+      frequencies.put(val, 0);
+    }
+    frequencies.put(val, frequencies.get(val) + 1);
   }
 
   private static void bug(String what) {
     Debug.out("Main", what);
-  }
-
-  public static Map<Integer, String> spaceMap = new HashMap<Integer, String>();
-
-  public static String spaces(int n) {
-    if (!spaceMap.containsKey(n)) {
-      StringBuffer buf = new StringBuffer();
-      for (int i = 0; i < n; i++) {
-        buf.append(" ");
-      }
-      spaceMap.put(n, buf.toString());
-    }
-    return spaceMap.get(n);
-  }
-
-  public static String bug(PythonTree t, boolean bugChildrenTypes) {
-    String childString = "" + t.getChildCount();
-
-    if (bugChildrenTypes) {
-      childString = "[";
-      for (int i = 0; i < t.getChildCount(); i++) {
-        childString += t.getChild(i).getToken().getText();
-        if (i < (t.getChildCount() - 1)) {
-          childString += ", ";
-        }
-      }
-      childString += "]";
-    }
-
-    String id = t.getText();
-
-    if (t instanceof Expr) {
-      Expr ex = (Expr) t;
-      // bug("blah " + ex.toStringTree());
-      // bug("attributes of expr: " + ex.get_attributes());
-      bug("Value of Expr: " + ex.getValue() + " (of class: " + ex.getValue().getClass() + ")");
-      if (ex.getValue() instanceof Call) {
-        Call c = (Call) ex.getValue();
-        bug("In call: func is: " + c.getFunc() + ("of class: " + c.getFunc().getClass()) + ")");
-        if (c.getFunc() instanceof Attribute) {
-          Attribute att = (Attribute) c.getFunc();
-          bugAttribute(att);
-        }
-      }
-    }
-    return t.getClass().toString() + " " + id + " line: " + t.getLine() + ", "
-        + t.getCharStartIndex() + "-" + t.getCharStopIndex() + " col: " + t.getCharPositionInLine()
-        + " type: " + PythonParser.tokenNames[t.getAntlrType()] + " children: " + childString;
-  }
-
-  public static void bugAttribute(Attribute att) {
-    bug(att.getAttr() + " (of class " + att.getAttr().getClass() + ") = " + att.getValue()
-        + " (of class: " + att.getValue().getClass() + ")");
-    if (att.getAttr() != null && att.getAttr() instanceof Attribute) {
-      bugAttribute((Attribute) att.getAttr());
-    }
-    if (att.getValue() != null && att.getValue() instanceof Attribute) {
-      bugAttribute((Attribute) att.getValue());
-    }
-  }
-
-  public static void processTree(PythonTree t, int indent) {
-    if (t instanceof Call) {
-      Call c = (Call) t;
-
-    }
-    System.out.println(spaces(3 * indent) + bug(t, true));
-
-    for (int i = 0; i < t.getChildCount(); i++) {
-      processTree(t.getChild(i), indent + 1);
-    }
   }
 
 }
